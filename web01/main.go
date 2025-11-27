ad14220b6e90e22480456a88a7d821e0d8e2b5c7
up1 <somkiat.p@gmail.com>
2025-11-27 17:07:29 +0700

    Add fatal git add .

--- web01/main.go
+++ web01/main.go
@@ -24,6 +24,10 @@ func main() {
 		panic("Pong !!!")
 	})
 
+	r.GET("/error", func(c *gin.Context) {
+		log.Fatal("Bye bye")
+	})
+
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
 	if err := r.Run(); err != nil {
