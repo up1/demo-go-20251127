4a77dc8f527dd2c8eabe10ad4551ca06c53d5674
up1 <somkiat.p@gmail.com>
2025-11-27 11:37:06 +0700

    Use testify package

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,13 +3,13 @@ package demo_test
 import (
 	"demo"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHelloSuccess(t *testing.T) {
 	actual := demo.SayHi()
 	expected := "Hello Go 2025"
 
-	if actual != expected {
-		t.Errorf("Expected %s but got %s", expected, actual)
-	}
+	assert.Equal(t, expected, actual)
 }
