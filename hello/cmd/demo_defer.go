e6edd173f87fa225ed65f1ff918e08c5864fcff0
up1 <somkiat.p@gmail.com>
2025-11-27 16:24:40 +0700

    Demo with defer

---
+++ hello/cmd/demo_defer.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	defer func() {
+		fmt.Println("Inner defer")
+	}()
+	fmt.Println("Finish")
+}
