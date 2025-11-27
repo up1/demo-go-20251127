72d246dbfbbe451ab273ac849cdfb8298b841160
up1 <somkiat.p@gmail.com>
2025-11-27 14:51:53 +0700

    Demo with testable

--- hello/my_random.go
+++ hello/my_random.go
@@ -5,13 +5,17 @@ import (
 	"math/rand"
 )
 
+type XXX interface {
+	GetNumber() int
+}
+
 type MyRandom struct{}
 
 func (mr MyRandom) GetNumber() int {
 	return rand.Intn(10) + 1
 }
 
-func GenerateData() string {
-	randomNumber := MyRandom{}.GetNumber()
+func GenerateData(x XXX) string {
+	randomNumber := x.GetNumber()
 	return fmt.Sprintf("My Data %d", randomNumber)
 }
--- hello/my_random_test.go
+++ hello/my_random_test.go
@@ -5,8 +5,14 @@ import (
 	"testing"
 )
 
+type MyStubRandom struct{}
+
+func (mr MyStubRandom) GetNumber() int {
+	return 5
+}
+
 func TestRamdomEqual5(t *testing.T) {
-	data := demo.GenerateData()
+	data := demo.GenerateData(MyStubRandom{})
 	expected := "My Data 5"
 	if data != expected {
 		t.Errorf("Expected %s but got %s", expected, data)
