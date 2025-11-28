0aaaf90bcf9b8395fb697164488346c949bf5ed8
up1 <somkiat.p@gmail.com>
2025-11-28 10:08:02 +0700

    Refactor 2

--- day2-api/main.go
+++ day2-api/main.go
@@ -6,14 +6,24 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type resource struct {
+	message string
+}
+
 func main() {
-	msg := "Hello, World!"
+	r := resource{message: "Hello, World v2!"}
 
 	e := echo.New()
-	e.GET("/", hello(msg))
+	e.GET("/", hello("V1 Hello, World!"))
+	e.GET("/v2", r.hello2)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+func (r resource) hello2(c echo.Context) error {
+	return c.String(http.StatusOK, r.message)
+}
+
+// Solution 1
 func hello(message string) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return c.String(http.StatusOK, message)
