6a552a17512bc337f95d81e25afabd2c6ac568ac
up1 <somkiat.p@gmail.com>
2025-11-28 09:51:42 +0700

    Refactor

--- day2-api/main.go
+++ day2-api/main.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
