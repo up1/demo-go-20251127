b378eb3068457a18bc7e0ae05fdd89e1c8689321
up1 <somkiat.p@gmail.com>
2025-11-28 10:01:38 +0700

    Send data to handler

--- day2-api/main.go
+++ day2-api/main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
+	msg := "Hello, World!"
+
 	e := echo.New()
-	e.GET("/", hello)
+	e.GET("/", hello(msg))
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func hello(message string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, message)
+	}
 }
